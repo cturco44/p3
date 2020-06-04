25f8fba3e195570cad706ca6201cff7d5bca0720
cturco <cturco@umich.edu>
2020-06-03 21:31:56 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Project Identifier: 01BD41C3BF016AD7E8B6F837DF18926EC3E83350
+//  main.cpp
+//  p3
+//
+//  Created by Connor Turco on 6/3/20.
+//  Copyright © 2020 Connor Turco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
