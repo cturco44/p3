76a2e2d8259984f96cee662c8ecfb6dd16a92d1d
cturco <cturco@umich.edu>
2020-06-05 01:28:22 -0400

    fix ss

--- output_test.sh
+++ output_test.sh
@@ -9,21 +9,21 @@
 make logman
 echo ============================ Test 1 =====================================
 echo -------------------------------------------------------------------------
-./logman test-1-log.txt < test-1-cmds.txt > testy.txt
+./logman_debug test-1-log.txt < test-1-cmds.txt > testy.txt
 sdiff testy.txt test1correct.txt
 echo -------------------------------------------------------------------------
 echo |
 echo |
 echo ============================ Spec Test ===================================
 echo -------------------------------------------------------------------------
-./logman spec-test-log.txt < spec-test-cmds.txt > testy.txt
+./logman_debug spec-test-log.txt < spec-test-cmds.txt > testy.txt
 sdiff testy.txt spec-test-out.txt
 echo -------------------------------------------------------------------------
 echo |
 echo |
 echo ============================ Sample All ===================================
 echo -------------------------------------------------------------------------
-./logman sample-log.txt < sample-all-cmds.txt > testy.txt
+./logman_debug sample-log.txt < sample-all-cmds.txt > testy.txt
 diff testy.txt sample-all-out.txt
 echo -------------------------------------------------------------------------
 make clean
